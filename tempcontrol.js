f395faa9ab88ae97a9fe47e25d11f03cf6fa8f6b
cvicens <carlos.vicens.alonso@gmail.com>
2016-01-25 07:44:34 +0100

    Adding Heating module

--- tempcontrol.js
+++ tempcontrol.js
@@ -5,21 +5,37 @@ var app = express();
 var util = require('util');
 var eep = require('eep');
 
+var heatingTimeout = null;
+
+var Heating = require('./lib/heating');
+var heating = new Heating();
+
+var DEFAULT_TIMER = 1; // TODO REVIEW!!!!
+
+var BASE_LINE_TEMP = 20;
+var BASE_LINE_TOLERANCE = 0.1;
+
 var monotonics = {};
 var monotonic_avg = eep.EventWorld.make().windows().monotonic(eep.Stats.all, new eep.CountingClock());
 monotonic_avg.on('emit', function(values) {
+  var hi_temp = BASE_LINE_TEMP + BASE_LINE_TOLERANCE;
+  var lo_temp = BASE_LINE_TEMP - BASE_LINE_TOLERANCE;
+
   if (values['mean'] != 0) {
     console.log("AVG (all) => " + JSON.stringify(values));
     if (values['mean'] != 0) {
-      if (values['mean'] < 20) {
-        console.log("turn ON heating!");
+      if (values['mean'] < lo_temp ) {
+        console.log("turn ON heating! " + values['mean'] + ' <= ' + lo_temp);
+        turnOnHeating();
+      } else if (values['mean'] > hi_temp) {
+        console.log("turn OFF heating! " + values['mean'] + ' > ' + hi_temp);
+        turnOffHeating();
       } else {
-        console.log("turn OFF heating!");
+        console.log('Do nothing! ' + lo_temp + ' <= ' + values['mean'] + ' <= ' + hi_temp);
       }
     }
   }
 });
-
 
 var intervalTickAvg = setInterval(monotonic_avg.tick, 10000);
 
@@ -48,6 +64,34 @@ function manageTemperatureMeasurement (sensorId, temp, presence) {
   monotonics[sensorId].enqueue(temp);
 }
 
+function turnOnHeating (minutes) {
+  if (typeof minutes === 'undefined' || minutes < 0) {
+     minutes = DEFAULT_TIMER;
+  }
+
+  // Set a timout for X minutes, calcel previous timer
+  if (heatingTimeout) {
+    clearTimeout(heatingTimeout);
+  }
+
+  heatingTimeout = setTimeout(function() {
+    console.log('FINER: time out reached!');
+    // Stop the heating
+    heating.off ();
+  }, minutes * 60 * 1000);
+
+  // turn on the heating
+  heating.on();
+}
+
+function turnOffHeating () {
+  // clear timer
+  clearTimeout(heatingTimeout);
+
+  // turn off the heating
+  heating.off();
+}
+
 // temp measurement
 app.get('/temp/:sensorId/:temp/:presence', function (req, res) {
   var temp = new Number(req.params.temp);
@@ -62,12 +106,56 @@ app.get('/temp/:sensorId/:temp/:presence', function (req, res) {
 // temp measurement
 app.get('/temp/:sensorId/:temp', function (req, res) {
   var temp = new Number(req.params.temp);
-  var presence = req.params.presence == "true";
+  var presence = false;
   var sensorId = req.params.sensorId;
 
   manageTemperatureMeasurement(sensorId, temp, presence);
 
   res.send({ name: req.params.sensorId, temp: req.params.temp });
+});
+
+// Heting command: on/off
+app.put('/heating/:cmd/:minutes*?', function (req, res) {
+  var cmd = req.params.cmd;
+  var minutes = req.params.minutes;
+  console.log('FINE: /heating/' + cmd + '/' + minutes);
+  if (typeof minutes === 'undefined') {
+     minutes = DEFAULT_TIMER;
+  }
+
+  var resp = null, desc = null;
+  if (cmd == 'on' || cmd == 'ON') {
+      if (minutes > 0) {
+        turnOnHeating (minutes);
+        resp = 'ACK';
+        desc = 'Heating ON';
+      } else {
+        // Bad cmd
+        resp = 'NACK';
+        desc = 'Bad command: ' + cmd + ' minutes: ' + minutes;
+      }
+  } else if (cmd == 'off' || cmd == 'OFF') {
+      turnOffHeating ();
+      resp = 'ACK';
+      desc = 'Heating OFF';
+  } else {
+      // Bad cmd
+      resp = 'NACK';
+      desc = 'Bad command: ' + cmd + ' minutes: ' + minutes;
+  }
+
+  res.send({ cmd: cmd, resp: resp, desc: desc });
+});
+
+// Heting command: on/off
+app.get('/heating/status', function (req, res) {
+  var response = null, desc = null;
+  var resp = null, desc = null;
+  var status = heating.status();
+  resp = 'ACK';
+  desc = 'Heating ' + (status == 1 ? 'ON' : 'OFF');
+
+  res.send({ status: status, resp: resp, desc: desc });
 });
 
 app.listen(3000);
---
+++ test.js
@@ -0,0 +1,36 @@
+var Relay = require('rpi-relay');
+var r1 = new Relay(14);
+var r2 = new Relay(15);
+
+r1.off();
+r2.on();
+
+console.log('antes setInterval');
+var iv = setInterval(function () {
+  var statusR1 = r1.readSync() ^ 1;
+  console.log('r1 ' + statusR1);
+
+  if (statusR1 == 1) {
+    r1.on();
+  } else {
+    r1.off();
+  }
+
+  var statusR2 = r2.readSync() ^ 1;
+  console.log('r2 ' + statusR2);
+
+  if (statusR2 == 1) {
+    r2.on();
+  } else {
+    r2.off();
+  }
+
+}, 5000);
+
+console.log('interval = ' + iv);
+
+// Stop blinking the relay and turn it off after 5 seconds.
+setTimeout(function () {
+  clearInterval(iv); // Stop blinking
+  r1.destroy();
+}, 15000);
