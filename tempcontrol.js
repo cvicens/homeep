2bca6d45e1518ecfd0eba8af1da8361fdcdef46d
cvicens <carlos.vicens.alonso@gmail.com>
2016-02-21 09:34:50 +0100

    Adding Heating module

--- tempcontrol.js
+++ tempcontrol.js
@@ -159,3 +159,21 @@ app.get('/heating/status', function (req, res) {
 });
 
 app.listen(3000);
+
+//////
+function exitHandler(options, err) {
+    if (options.cleanup) {
+      console.log('FINE: cleaning');
+      heating.destroy();
+    }
+    if (err) console.log(err.stack);
+    if (options.exit) process.exit();
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+//////
